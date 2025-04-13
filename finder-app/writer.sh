44b92ec4d42b42c5ac8ba935ed4076eb8db22383
sanal <sanal.chelattu@gmail.com>
2025-04-13 19:20:56 +0530

    write updte

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,5 +1,5 @@
 
-DIRECTORY=$1
+ABS_FILE=$1
 KEY=$2
 
 if [ -z "$1" ] || [ -z "$2" ]; then
@@ -7,9 +7,20 @@ if [ -z "$1" ] || [ -z "$2" ]; then
      exit 1
 fi
 
+dir_path=$(dirname "$ABS_FILE")
 
-if [ ! -d "$DIRECTORY" ]; then
-     echo "Error: Directory '$dir' does not exist."
-     exit 1
+if [ ! -d "$dir_path" ]; then
+        mkdir -p "$dir_path" || {
+            echo "Error: Failed to create directory '$dir_path'" >&2
+            exit 1
+     }
 fi
 
+touch "$ABS_FILE" || {
+     echo "Error: Failed to create file '$ABS_FILE'" >&2
+     exit 1
+}
+
+echo "writestr" > "$ABS_FILE"
+
+echo "File created: $ABS_FILE"
