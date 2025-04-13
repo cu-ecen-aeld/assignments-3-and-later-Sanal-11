1ba3bd773724f87b39a4f64a1fa82f4ce3079168
sanal <sanal.chelattu@gmail.com>
2025-04-13 17:02:55 +0530

    finder-app validation error

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -39,13 +39,13 @@ KEY=$2
 
 if [ -z "$1" ] || [ -z "$2" ]; then
      echo "Error: Parameters are empty."
-     exit 0
+     exit 1
 fi
 
 
 if [ ! -d "$DIRECTORY" ]; then
      echo "Error: Directory '$dir' does not exist."
-     exit 0
+     exit 1
 fi
 
 
