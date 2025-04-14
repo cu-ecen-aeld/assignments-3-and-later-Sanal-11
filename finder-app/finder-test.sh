c9e710abe94909777d4ce3614954632f8babec02
sanal <sanal.chelattu@gmail.com>
2025-04-15 01:21:53 +0530

    finder-test error handling

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-DIRECTORY=$1
-KEY=$2
-
-
 make $1
 
+if [ ! -x ./writer ]; then
+    echo "Error: writer not found or not executable"
+    exit 1
+fi
+
+./writer testfile.txt "testing word"
+rc=$?
+
+if [ $rc -ne 0 ]; then
+    exit 1
+fi
